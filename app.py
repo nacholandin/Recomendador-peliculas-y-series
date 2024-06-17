97e7b1b24caed9e85f1b01d5672c4409aa7dda20
nacho <ignaciolandin@hotmail.com>
2024-06-17 13:27:37 +0200

    actualizacion

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import plotly.express as px
+from PIL import Image
 import numpy as np
-
+import os
+#from module.ml_func import
 from eda import eda_app
 #from ml import ml_app
 from about import about_app
@@ -27,6 +29,20 @@ def main():
         st.write("""To use the `Machine Learning Model` section you can either use the sliders in the sidebar or upload you own CSV file.""")
 
         st.warning("""Note: If you are using a CSV file you cannot use the sidebar's sliders to use the model.""") 
+        image1 = Image.open("source/Neflix.jpg")
+        image2 = Image.open("source/hbo.jpg")
+        image3 = Image.open("source/disney-tile-gradient_007bce85.jpeg")
+        image4 = Image.open("source/amazonprime.png")
+        image5 = Image.open("source/paramount.png")
+        images = [image1, image2, image3, image4, image5]
+        num_columns = 3
+        cols = st.columns(num_columns)
+        for idx, image in enumerate(images):
+            cols[idx % num_columns].image(image, use_column_width=True)
+        #st.image(image           = image,
+        #caption          = "Neflix",
+         #use_column_width = True)
+
        
     elif choice == "Exploratory Data Analysis":
         eda_app()
---
+++ ml.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+#from modules.ml_func import *
+import plotly.express as px
+#import statsmodels
+# pip install statsmodels
+
+
+
+def ml_app():
+    
+    st.subheader(body = "Machine Learning Model :robot_face:")
+
+    
+
+
+if __name__ == "__ml_app__":
+    eda_app()
+
+
