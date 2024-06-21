c00c228766ef0e13347ed1c9339fcecf8ec548e0
nacho <ignaciolandin@hotmail.com>
2024-06-21 14:37:39 +0200

    avances

--- ml.py
+++ ml.py
@@ -1,21 +1,92 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#from modules.ml_func import *
-import plotly.express as px
-#import statsmodels
-# pip install statsmodels
+from module.ml_func import *
+
 
 
 
 def ml_app():
     
-    st.subheader(body = "Machine Learning Model :robot_face:")
-
+    st.subheader(body = "RECOMENDADOR")
+    st.write("En esta sección puedes comprobar como funciona el recomendador, tienes que valorar un mínimo de 5 peliculas o series con una puntación entre 1-5.")
+    df = read_data()
+    # if 'valoraciones_df' not in st.session_state:
+    #     st.session_state['valoraciones_df'] = pd.DataFrame(columns=["title", "rating"])
+    # peliculas =["Buscar pelicula o serie"] + list(df["title"].unique())
+    # choice_peliculas = st.multiselect(label = "Peliculas", options = peliculas)
     
+    # valoracion = [""] + ['1','2','3','4','5']
+    # choice = st.selectbox(label = "Puntación", options = valoracion)
+    
+    # if choice_peliculas and valoracion:
+    #     for pelicula in choice:
+    #         st.session_state['valoraciones_df'] = st.session_state['valoraciones_df'].append({"title": pelicula, "rating": valoracion}, ignore_index=True)
+    
+    # if choice_peliculas and valoracion:
+    #     st.write(f"Has seleccionado las siguientes películas: {', '.join(choice)}")
+    #     st.write(f"Tu valoración para estas películas es: {valoracion}")
+
+    # if not st.session_state['valoraciones_df'].empty:
+    #     st.write("Valoraciones Guardadas:")
+    #     st.dataframe(st.session_state['valoraciones_df'])
+    # else:
+    #      st.write("Aún no has guardado ninguna valoración.")
+
+    if 'valoraciones_df' not in st.session_state:
+        st.session_state['valoraciones_df'] = pd.DataFrame(columns=["title", "rating"])
+
+    peliculas = ["Buscar película o serie"] + list(df["title"].unique())
+
+    choice = st.multiselect(label="Películas", options=peliculas)
+
+    valoracion = st.selectbox(label="Puntuación", options=['', '1', '2', '3', '4', '5'])
+
+    if choice and valoracion:
+        nuevos_ratings = [{"title": pelicula, "rating": valoracion} for pelicula in choice]
+        st.session_state['valoraciones_df'] = pd.concat([st.session_state['valoraciones_df'], pd.DataFrame(nuevos_ratings)], ignore_index=True)
+
+    if choice and valoracion:
+        st.write(f"Has seleccionado las siguientes películas: {', '.join(choice)}")
+        st.write(f"Tu valoración para estas películas es: {valoracion}")
+
+    if not st.session_state['valoraciones_df'].empty:
+        st.write("Valoraciones Guardadas:")
+        st.dataframe(st.session_state['valoraciones_df'])
+    else:
+        st.write("Aún no has guardado ninguna valoración.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__ml_app__":
-    eda_app()
+    ml_app()
 
 
