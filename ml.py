b0dd816f68e61ba498bc933eb39042abaff701a5
nacho <ignaciolandin@hotmail.com>
2024-06-26 12:39:20 +0200

    cambios

--- ml.py
+++ ml.py
@@ -254,9 +254,9 @@ def ml_app():
             )
                 else:
                         return (
-                            0.77 * (row['Puntuacionge'] - dfre['Puntuacionge'].min()) / (dfre['Puntuacionge'].max() - dfre['Puntuacionge'].min()) +
-                            0.10 * row["Puntuacionac"] / dfre["Puntuacionac"].max()+
-                            0.13 *(row['imdb_score'] - dfre['imdb_score'].min()) / (dfre['imdb_score'].max() - dfre['imdb_score'].min())
+                            0.81 * (row['Puntuacionge'] - dfre['Puntuacionge'].min()) / (dfre['Puntuacionge'].max() - dfre['Puntuacionge'].min()) +
+                            0.11 * row["Puntuacionac"] / dfre["Puntuacionac"].max()+
+                            0.08 *(row['imdb_score'] - dfre['imdb_score'].min()) / (dfre['imdb_score'].max() - dfre['imdb_score'].min())
             )
 
             dfre["Puntuacion"] = dfre.apply(calcular_puntuacion, axis=1)
